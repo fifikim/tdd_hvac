b617006f20c6f423d4d37fca7e3bb600fc23bb52
Sophia Kim <kims@shure.com>
2023-03-28 10:19:57 -0700

    update thermostat class

--- src/hvac_elements/thermostat.js
+++ src/hvac_elements/thermostat.js
@@ -1,6 +1,8 @@
 import HvacElement from "./hvac_element.js";
 
 export default class Thermostat extends HvacElement {
+
+  // which dependencies should we inject?
   constructor() {
     super();
     this.name = "thermostat";
@@ -8,10 +10,29 @@ export default class Thermostat extends HvacElement {
     this._temp = 72;
   }
 
-  get temp() {
-    return this._temp;
-  }
-
-  // add methods
+    // implement these methods
+    setHeat() {} // True turns the heater on, false turns it off
+    setCool() {} // True turns the cooler on, false turns it off
+    setFan() {} // True turns the fan on, false turns it off
+    getTemp() {
+      return this._temp;
+    } // Get the temperature of the room (as an int)
+  
+    // finish this method
+    tick() {
+      setInterval(() => {
+        this.#currentSystemStatus();
+        
+        // what goes here?
+        this.fan.explode();
+  
+      }, 2000);
+    }
+  
+    #currentSystemStatus() {
+      console.log("tick");
+  
+      // what else?
+    }
   
 }
